a1058c489a60732af77af832f812f7a5649ffeca
kriox26 <matias.pan26@gmail.com>
2015-10-12 15:20:37 -0300

    Agrego #pasarHabilitado() a Ventana e Irrompible

--- src/modelo/niceland/Irrompible.java
+++ src/modelo/niceland/Irrompible.java
@@ -10,4 +10,17 @@ public class Irrompible extends Ventana {
 	public void setAbierta(boolean abierta){
 		this.abierta = abierta;
 	}
+
+	/*
+	 * Para ventanas irrompibles, el unico obstaculo son las persianas
+	 * @return boolean
+	 * @Override
+	 */
+	protected boolean pasarHabilitado(){
+		if(this.getAbierta() == true){
+			return false;
+		} else {
+			return true;
+		}
+	}
 }
--- src/modelo/niceland/Ventana.java
+++ src/modelo/niceland/Ventana.java
@@ -3,7 +3,14 @@ package modelo.niceland;
 public class Ventana {
 
 
+	/*
+	 * Esto va a devolver true si la ventana a la
+	 * que se esta llamando es simple o semicircular.
+	 * En caso de que sea irrompible, se va a acceder
+	 * al metodo de la clase correspondiente
+	 * @return boolean
+	 */
 	protected boolean pasarHabilitado(){
-		return false;
+		return true;
 	}
 }

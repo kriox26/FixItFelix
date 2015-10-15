ec4c8fe5e48e2c1bd4541da25d1e52684658d9ce
kriox26 <matias.pan26@gmail.com>
2015-10-15 17:15:18 -0300

    Corrijo comentarios, saco whitespace

--- src/modelo/main/Main.java
+++ src/modelo/main/Main.java
@@ -1,35 +1,35 @@
 package modelo.main;
+import modelo.niceland.*;
 
 public class Main {
     private boolean jugando;
     public Desarrollo dvp;
-    
+
     public Main(boolean jugando){
         this.jugando = jugando;
     }
-    
+
     public boolean getJugando(){
         return this.jugando;
     }
-    
+
     public void setJugando(boolean jugando){
         this.jugando = jugando;
     }
-    
+
     public void jugar(){
-        this.inicializar(); // Para inicializar el todo
+        this.inicializar(); // Para inicializar todo(niceland TODO= personajes)
     }
-    
+
     // TODO: implementar todas las funciones
     private void inicializar(){
-        // Inicializamos el edificio Niceland
-        /*
-         * Se tendria que hacer algo asi:
-         * niceland = new Niceland(); Para iniciar el edificio, en el test se tiene un solo nivel
-         * Esa linea va a crear todas las secciones, ventanas, paneles, estados
-         * this.dvp = new Desarrollo(1, 0); // con nivel = 1, puntos = 0
-         * Cuando se inicializa la clase Desarrollo, se inicializan los personajes tambien
-         * la posicion inicial de Felix va a ser: Fila 0, columna 2(tomandolo con dimensiones de arreglos)
-         */
+        // Inicializamos el edificio Niceland, params: nivel = 1
+        // Se crean todas las secciones, ventanas, paneles, estados
+        niceland = new Niceland(1);
+        // this.dvp = new Desarrollo(0, 0); // con nivel actual = 0, puntos = 0
+        // Cuando se inicializa la clase Desarrollo, se inicializan los personajes tambien
+        this.dvp = new Desarrollo(0, 0);
+				// Habria que inicializar a los personajes, seria ideal que con llamar al new desarrollo
+				// se cree todo
     }
 }

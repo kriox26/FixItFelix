8d1b558c7cefcef9fdfc7b3f0663c0c206a0657d
santimarrone <santiagocmarrone@gmail.com>
2015-10-17 19:44:14 -0300

    Nicelander avanzado y archivado
    Not necessary for test so... bye bye mother fucker. Every line en public
    class Nicelander has been commented to avoid filling of shit the hole
    program. See you nigga.

--- src/modelo/dinamica/personajes/Nicelander.java
+++ src/modelo/dinamica/personajes/Nicelander.java
@@ -1,5 +1,50 @@
 package modelo.dinamica.personajes;
 
-public class Nicelander {
+import modelo.niceland.*;
+import modelo.dinamica.*;
 
+public class Nicelander { /*
+	private boolean tieneTarta;
+	private boolean apareciendo;
+	private Posicion posicion;
+
+	public Posicion getPosicion() {
+		return this.posicion;
+	}
+
+	public void setPosicion (Posicion variable) {
+		this.posicion = variable;
+	}
+
+	public boolean getTieneTarta () {
+		return this.tieneTarta;
+	}
+
+	public void setTieneTarta (boolean variable) {
+		this.tieneTarta = variable;
+	}
+
+	public boolean getApareciendo () {
+		return this.apareciendo;
+	}
+
+	public void setApareciendo (boolean variable) {
+		this.apareciendo = variable;
+	}
+
+	public void aparecer () {
+		this.apareciendo = true;
+	}
+
+	public void desaparecer () {
+		this.apareciendo = false;
+	}
+
+	private boolean tartaOK () {
+		return true;
+	}
+
+	public void ponerTarta () {
+		
+	} */
 }

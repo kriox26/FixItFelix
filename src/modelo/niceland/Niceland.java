471862dac2146204174e1acbca5c87ebeb5b1791
kriox26 <matias.pan26@gmail.com>
2015-10-17 15:48:02 -0300

    Arreglo caso extremo

--- src/modelo/niceland/Niceland.java
+++ src/modelo/niceland/Niceland.java
@@ -11,13 +11,11 @@ public class Niceland {
      * @params int niveles: Niveles del juego
      */
 	public Niceland(int niveles){
-        // niveles * 3 es la cantidad de secciones total. Hay 3 pisos por seccion
         int total_secciones = niveles * 3;
         secciones = new Seccion[total_secciones];
-        // Parametros: String tipo, boolean piso, boolean techo
         secciones[0] = new Seccion("Inferior", true, false);
         secciones[total_secciones - 1] = new Seccion("Superior", false, true);
-        for (int i = 1; i <= total_secciones; i++) {
+        for (int i = 1; i < total_secciones; i++) {
             secciones[i] = new Seccion("Media", false, false);
         }
 	}

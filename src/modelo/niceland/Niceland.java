4fe7afb81e8f26d6b0b3c968f169781b9a699082
kriox26 <matias.pan26@gmail.com>
2015-10-17 18:33:31 -0300

    Metodo rastrear para usar en personajes

--- src/modelo/niceland/Niceland.java
+++ src/modelo/niceland/Niceland.java
@@ -1,8 +1,18 @@
 package modelo.niceland;
+import modelo.dinamica.Posicion;
 
 public class Niceland {
 	private Seccion[] secciones;
+    private int seccion_actual;
 	
+    public int getSeccionActual(){
+        return this.seccion_actual;
+    }
+    
+    public void setSeccionActual(int seccion_actual){
+        this.seccion_actual = seccion_actual;
+    }
+
     /*
      * Constructor, recibe un entero que indica los niveles del juego, lo cual
      * tambien indica la cantidad de secciones que va a haber. Si el juego es de
@@ -18,5 +28,17 @@ public class Niceland {
         for (int i = 1; i < total_secciones; i++) {
             secciones[i] = new Seccion("Media", false, false);
         }
+        seccion_actual = 0;
 	}
+    
+    /*
+     * Devuelve la ventana en la que felix esta actualmente
+     * @param posicion : Es la posicion en la que esta felix
+     * @return Ventana : Ventana en la que esta parado felix
+     */
+    public Ventana rastrear(Posicion posicion){
+        return secciones[this.getSeccionActual()].ventanas[fila][columna];
+    }
+    
+    
 }

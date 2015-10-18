ee60b72550dae07802ed07d331eb80e89d24b895
santimarrone <santiagocmarrone@gmail.com>
2015-10-18 02:06:05 -0300

    ATENTION Read description please
    Hay un tema: cuando quiero indicarle una dirección particular a las
    cosas (por ejemplo, decirle al señor Ladrillo que debe direccionarse
    hacia abajo) he tenido problemas y por eso comenté los métodos.

--- src/modelo/dinamica/objetos/Objeto.java
+++ src/modelo/dinamica/objetos/Objeto.java
@@ -6,6 +6,8 @@ import modelo.dinamica.Posicion;
 
 public class Objeto extends Dinamico {
 	private boolean existe;
+	private double velocidad;
+	private static final double kv = 1;
 	
 	public boolean getExiste(){
 		return this.existe;
@@ -28,6 +30,18 @@ public class Objeto extends Dinamico {
 	public void aparecer (Posicion position) {
 		this.setExiste (true);
 		this.setPosicion(position);
+	}
+	
+	public double getVelocidad () {
+		return this.velocidad;
+	}
+
+	public void setVelocidad (double variable) {
+		this.velocidad = variable;
+	}
+
+	public void actualizarVelocidad (int nivel) {
+		this.setVelocidad(nivel * kv);
 	}
 
 }
--- src/modelo/dinamica/personajes/Ralph.java
+++ src/modelo/dinamica/personajes/Ralph.java
@@ -1,28 +1,83 @@
 package modelo.dinamica.personajes;
+
 import modelo.dinamica.Posicion;
+import modelo.direcciones.*;
 
 public class Ralph extends Personaje {
 	private int ladrillos;
-	private static int cuantosTira = 1;
-    
-    public Ralph(String nombre, int ladrillos, Posicion posicion){
-    	super(nombre, posicion);
-        this.ladrillos = ladrillos;
-    }
-	
+	private static final int cuantosTira = 1;
+	private static final int kl = 5; // Constante de ladrillos por nivel
+
+  public Ralph(String nombre, int ladrillos, Posicion posicion){
+  	super(nombre, posicion);
+      this.ladrillos = ladrillos;
+  }
+
 	public int getLadrillos () {
 		return this.ladrillos;
 	}
-	
+
 	public void setLadrillos (int cantidad) {
 		this.ladrillos = cantidad;
 	}
-	
-	public boolean hayLadrillos () {
+
+	private boolean hayLadrillos () {
 		if (this.getLadrillos() >= cuantosTira) {
 			return true;
 		}
 		else
 			return false;
 	}
+	
+	public void actualizarLadrillos (int nivel) {
+		this.setLadrillos(nivel * kl);
+	}
+
+	public void tirarLadrillos () {
+		this.setLadrillos(this.getLadrillos() - cuantosTira);
+		for (int i=0; i<= cuantosTira; i++) {
+			// ladrillo.caerDesde(this.getPosicion());
+		}
+	}
+	
+	private boolean alBordeIzquierdo () {
+		if (this.getPosicion().getColumna() == 1) {
+			return true;
+		}
+		else 
+			return false;
+	}
+	
+	private boolean alBordeDerecho () {
+		if (this.getPosicion().getColumna() == 5) {
+			return true;
+		}
+		else 
+			return false;
+	}
+	
+	private boolean alBorde () {
+		if (this.alBordeDerecho() || this.alBordeIzquierdo()) {
+			return true;
+		}
+		else 
+			return false;
+	}
+	
+	private void movimientoParcial (Direccion direction) {
+		while (!this.alBorde()) {
+			this.mover(direction);
+		}
+	}
+	
+	/* public void movimientoTotal () { // There's a fucking issue with the direction task
+		if (this.alBorde()) {
+			if (this.alBordeIzquierdo()){
+				this.movimientoParcial(derecha);
+			}
+			else {
+				this.movimientoParcial(izquierda);
+			}
+		}
+	} */
 }

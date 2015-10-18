8ab1953c6937544674e228c80470bcdef8a0faa4
kriox26 <matias.pan26@gmail.com>
2015-10-18 18:21:26 -0300

    Modificaciones para el test

--- src/modelo/dinamica/objetos/Ladrillo.java
+++ src/modelo/dinamica/objetos/Ladrillo.java
@@ -6,9 +6,33 @@ import modelo.dinamica.Posicion;
 public class Ladrillo extends Objeto {
 	
 	private static final double kv = 9.58; // Constante de variaciÃ³n de velocidad
+    
+    public Ladrillo(){
+        Posicion posicion = new Posicion();
+        super.setPosicion(posicion);
+    }
+    
+    public Ladrillo(Posicion posicion){
+        posicion.setSeccion(posicion.getSeccion() - 1);
+        posicion.setFila(2);
+        super.setPosicion(posicion);
+    }
 	
 	public void caerDesde (Posicion position) {
 		this.aparecer(position); // Crearlo
-		this.unidireccionar(Direccion.ABAJO); // Unidireccionarlo hacia abajo (en niveles Pro podría alterarse la gravedad)
+		this.unidireccionar(Direccion.ABAJO); // Unidireccionarlo hacia abajo (en niveles Pro podrï¿½a alterarse la gravedad)
 	}
+    
+    public void mover(Direccion direccion){
+        if (this.getPosicion().getSeccion() == 0) {
+            if (this.getPosicion().getFila() > 0) {
+                super.mover(direccion);
+            }
+        }
+    }
+    
+    public void caer(){
+        super.mover(Direccion.ABAJO);
+        System.out.println("El ladrillo cae, su posicion es: (Seccion: " + this.getPosicion().getSeccion() + ", Fila: " + this.getPosicion().getFila() + ", Columna: " + this.getPosicion().getColumna() + ")");
+    }
 }

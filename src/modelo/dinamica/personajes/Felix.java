b4eaedd27478943613d1231db8fe778cb61d2589
santimarrone <santiagocmarrone@gmail.com>
2015-10-18 00:32:26 -0300

    Acción REPARAR + MARTILLAR

--- src/modelo/dinamica/personajes/Felix.java
+++ src/modelo/dinamica/personajes/Felix.java
@@ -5,41 +5,41 @@ import modelo.niceland.*;
 public class Felix extends Personaje{
 	private int vida;
 	private boolean inmunidad;
-	
+
     public Felix(String nombre, int vida, boolean inmunidad, Posicion posicion){
     	super(nombre, posicion);
         this.vida = vida;
         this.inmunidad = inmunidad;
     }
-    
+
 	public int getVida(){
 		return this.vida;
 	}
-	
+
 	public void setVida (int variable){
 		this.vida = variable;
 	}
-	
+
 	public boolean getInmunidad() {
 		return this.inmunidad;
 	}
-	
+
 	public void setInmunidad (boolean variable){
 		this.inmunidad = variable;
 	}
-	
+
 	public boolean estaInmune () {
         return this.getInmunidad() ? true : false;
 	}
-	
+
 	public void activarInmunidad () {
 		this.inmunidad = true;
 	}
-	
+
 	public void desactivarInmunidad () {
 		this.inmunidad = false;
 	}
-	
+
 	public void golpeadoPorLadrillo () {
 		if (this.estaInmune()) {
 			this.desactivarInmunidad();
@@ -47,21 +47,27 @@ public class Felix extends Personaje{
 		this.setVida (this.getVida()-1);
 		this.recomenzar();
 	}
-	
+
 	public void golpeadoPorPaloma (){
 		this.recomenzar();
 	}
-	
+
 	public void recomenzar () {
 		// Vuelve a empezar de la misma pantalla... WTF?
 	}
-	
+
 	public void comerTarta () {  // Debe ejecutarse si en Felix.posicion hay un sabroso pastel
 		this.activarInmunidad();
 		// Hacer desaparecer la tarta de donde est�
 	}
-    
-    public void martillar(Ventana ventana){
-        ventana.arreglarPanel();
-    }
+
+  private void reparar (Ventana ventana){
+      ventana.arreglarPanel();
+  }
+
+	public void martillar (Ventana ventana) {
+		if ventana.estaSana(){
+			this.reparar(ventana);
+		}
+	}
 }

d39f29bfd37351265df7600c4a2c573d260badd7
kriox26 <matias.pan26@gmail.com>
2015-11-25 09:55:46 -0300

    Imagenes de fondo y back button p/instrucciones, highscores

--- src/grafica/menu/Instrucciones.java
+++ src/grafica/menu/Instrucciones.java
@@ -1,27 +1,46 @@
 package grafica.menu;
 
-import java.awt.Button;
+import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.File;
 
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class Instrucciones extends JFrame{
-	private Button goBack;
+	private JButton goBack;
+	private String imgPath = "src/grafica/imagenes/";
+	private String backgroundImage = imgPath + "guia.jpg";
+	private ImageIcon goBackIcon = new ImageIcon(imgPath + "back_arrow_blue.png");
 
 
 	public Instrucciones(){
-		setLayout(new FlowLayout(FlowLayout.CENTER, 100, 550));
-		goBack = new Button("Menu");
+		setLayout(new BorderLayout());
+//		setLayout(new FlowLayout(FlowLayout.CENTER, 100, 550));
+
+        File image = new File(backgroundImage);
+
+		setContentPane(new JLabel(new ImageIcon(image.getAbsolutePath())));
+
+		setLayout(new FlowLayout(FlowLayout.CENTER, 0, 10));
+
+		goBack = new JButton();
+		goBack.setIcon(goBackIcon);
+		goBack.setBorder(null);
 		add(goBack);
+
 		goBack.addMouseListener(new MouseAdapter(){
 			public void mouseClicked(MouseEvent arg0){
 				setVisible(false);
 				MainMenu mn = new MainMenu();
 			}
 		});
-		setLayout(new FlowLayout(FlowLayout.CENTER, 100, 550));
+
+//		setLayout(new FlowLayout(FlowLayout.CENTER, 100, 550));
 		setTitle("Instrucciones de Juego");
 		setSize(919, 720);
 		setVisible(true);
--- src/grafica/menu/TopScores.java
+++ src/grafica/menu/TopScores.java
@@ -1,25 +1,45 @@
 package grafica.menu;
 
+import java.awt.BorderLayout;
 import java.awt.Button;
 import java.awt.FlowLayout;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.File;
 
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class TopScores extends JFrame{
-	private Button goBack;
+	private JButton goBack;
+	private String imgPath = "src/grafica/imagenes/";
+	private String backgroundImage = imgPath + "high_scores.jpg";
+	private ImageIcon goBackIcon = new ImageIcon(imgPath + "back_arrow_blue.png");
 
 
 	public TopScores(){
-		setLayout(new FlowLayout(FlowLayout.CENTER, 100, 550));
-		goBack = new Button("Menu");
+		setLayout(new BorderLayout());
+
+        File image = new File(backgroundImage);
+
+		setContentPane(new JLabel(new ImageIcon(image.getAbsolutePath())));
+
+		setLayout(new FlowLayout(FlowLayout.LEFT, 0, 5));
+
+		goBack = new JButton();
+		goBack.setIcon(goBackIcon);
+		goBack.setBorder(null);
+		add(goBack);
+
 		goBack.addMouseListener(new MouseAdapter(){
 			public void mouseClicked(MouseEvent arg0){
 				setVisible(false);
 				MainMenu mn = new MainMenu();
 			}
 		});
+
 		add(goBack);
 		setTitle("Top Scores");
 		setSize(919, 720);

6182c111617d97011278a5535a058000e80c4009
Santiago Marrone <santiagocmarrone@gmail.com>
2015-11-25 11:37:57 -0300

    Oh, what a nice bird !
    Just to notify you guys I’m on this…

---
+++ src/grafica/dinamica/objetos/Paloma.java
@@ -0,0 +1,8 @@
+
+public class Paloma extends xxxx {
+	private String imgPath = "src/grafica/imagenes/pajaro";
+	private String derAA = imgPath + "slice08_08.png"; // der: hacia la derecha; AA: ala alta
+	private String derAB = imgPath + "slice09_09.png"; // AB: ala baja
+  private String izqAA = imgPath + "slice41_41.png"; // izq: hacia la izquierda
+  private String izqAB = imgPath + "slice61_61.png";
+}

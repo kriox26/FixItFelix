26fcb0ff5cd8aa734fdc03d08a9d487ffd254f12
Jorge <Jorch95@gmail.com>
2015-11-25 00:23:49 -0300

    Agregado Boton Menu con Imagen y lo pasé a JButton

--- src/grafica/menu/Instrucciones.java
+++ src/grafica/menu/Instrucciones.java
@@ -1,19 +1,24 @@
 package grafica.menu;
 
-import java.awt.Button;
 import java.awt.FlowLayout;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class Instrucciones extends JFrame{
-	private Button goBack;
+	private JButton goBack;
+	private ImageIcon goBackBoton = new ImageIcon("src/grafica/imagenes/Menu.png");
 
 
 	public Instrucciones(){
 		setLayout(new FlowLayout(FlowLayout.CENTER, 100, 550));
-		goBack = new Button("Menu");
+		goBack = new JButton();
+		goBack.setIcon(goBackBoton);
+		goBack.setBorder(null);
+		goBack.setContentAreaFilled(false);
 		add(goBack);
 		goBack.addMouseListener(new MouseAdapter(){
 			public void mouseClicked(MouseEvent arg0){
--- src/grafica/menu/TopScores.java
+++ src/grafica/menu/TopScores.java
@@ -1,19 +1,23 @@
 package grafica.menu;
 
-import java.awt.Button;
 import java.awt.FlowLayout;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class TopScores extends JFrame{
-	private Button goBack;
-
+	private JButton goBack;
+	private ImageIcon goBackBoton = new ImageIcon("src/grafica/imagenes/Menu.png");
 
 	public TopScores(){
 		setLayout(new FlowLayout(FlowLayout.CENTER, 100, 550));
-		goBack = new Button("Menu");
+		goBack = new JButton();
+		goBack.setIcon(goBackBoton);
+		goBack.setBorder(null);
+		goBack.setContentAreaFilled(false);
 		goBack.addMouseListener(new MouseAdapter(){
 			public void mouseClicked(MouseEvent arg0){
 				setVisible(false);

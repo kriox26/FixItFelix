79c5eda9d13576ae6bf7cb140ba0ec0e6d7da0bd
Santiago Marrone <santiagocmarrone@gmail.com>
2015-11-26 10:16:56 -0300

    Grua
    Máquina que aparece en la animación introductoria.

---
+++ src/modelo/dinamica/objetos/grua.java
@@ -0,0 +1,27 @@
+package modelo.dinamica.objetos;
+
+import modelo.direcciones.Direccion;
+import modelo.dinamica.Posicion;
+
+/*
+ * Representa la grua que aparece en las animaciones de introduccion al juego
+ */
+public class Grua extends Objeto {
+    private Direccion direccion;
+
+    public Direccion getDireccion(){
+        return this.direccion;
+    }
+
+    public void setDireccion(Direccion direccion){
+        this.direccion = direccion;
+    }
+
+    public void actualizar(){
+        desplazar();
+    }
+
+    private void desplazar(){
+        super.mover(this.getDireccion());
+    }
+}

719443f54bbf0fecd214bb035c69fcd21db914c4
Santiago Marrone <santiagocmarrone@gmail.com>
2015-11-27 14:35:15 -0300

    @author borrado @ RandomAcotado

--- src/modelo/util/RandomAcotado.java
+++ src/modelo/util/RandomAcotado.java
@@ -3,17 +3,15 @@ import java.util.Random;
 
 /*
  * Clase de utilidad. Genera un numero random en un rango acotado.
- * @author Santiago Marrone
- * @author Matias Pan
  */
 public class RandomAcotado {
 	private int numero;
-	
+
 	public RandomAcotado (int min, int max) {
         Random rand = new Random();
         this.numero = rand.nextInt((max - min) + 1) + min;
     }
-	
+
 	public int getValor () {
 		return this.numero;
 	}

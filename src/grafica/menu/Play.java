04d48b68def726a288634c5159b5c2b7cbd4af84
kriox26 <matias.pan26@gmail.com>
2015-11-27 17:21:55 -0300

    NullPointerException cuando .drawImage()

--- src/grafica/menu/Play.java
+++ src/grafica/menu/Play.java
@@ -2,11 +2,14 @@ package grafica.menu;
 
 import java.awt.Graphics;
 import java.awt.event.KeyAdapter;
-import java.awt.event.MouseAdapter;
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+import java.util.TreeMap;
 
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
-import javax.swing.JButton;
 import javax.swing.JLabel;
 
 import modelo.main.Main;
@@ -15,12 +18,14 @@ public class Play extends Grafica {
 	private String imgPath = "src/grafica/niceland/";
 	private String backgroundImage = imgPath + "FondoNiceland.jpg";
 	private JLabel fondo;
+	private Map<String, BufferedImage> imgs = new TreeMap<String, BufferedImage>();
 
 	/*
 	 * Se crean todas las imagenes de Niceland, ventanas con obstaculos
 	 * todo.
 	 */
 	public Play(Main main){
+		cargarImagenes();
 		File imgFondo = new File(backgroundImage);
 		JLabel fondo = new JLabel(new ImageIcon(imgFondo.getAbsolutePath()));
 		add(goBack);
@@ -36,11 +41,28 @@ public class Play extends Grafica {
 	}
 
 	public void cargarNiceland(){
-//		this.getGraphics().drawImage(imgs.get("seccion1"), 20, 0, null);
+		BufferedImage img = imgs.get("edificio_150_seccion1.png");
+		System.out.println("Imagen: " + img);
+		this.getGraphics().drawImage(imgs.get("edificio_150_seccion1.png"), 20, 0, null);
 	}
 
-	public void paint(Graphics g){
+	public void paintComponents(Graphics g){
 		super.paintComponents(g);
+	}
+
+	private void cargarImagenes(){
+		File folder = new File("src/grafica/imagenes/edificio/");
+		File[] filesList = folder.listFiles();
+
+		for(File img : filesList){
+			try {
+				BufferedImage imagen = ImageIO.read(img);
+				System.out.println("El nombre es:" + img.getName());
+				this.imgs.put(img.getName(), imagen);
+			} catch(IOException e){
+				e.printStackTrace();
+			}
+		}
 	}
 
 }

2156c22c068fadf0a8965b04f9f14d9981b3327b
Santiago Marrone <santiagocmarrone@gmail.com>
2015-11-30 00:38:38 -0300

    Construccion inteligente @ Paloma

--- src/modelo/dinamica/Posicion.java
+++ src/modelo/dinamica/Posicion.java
@@ -10,42 +10,42 @@ import modelo.direcciones.*;
 public class Posicion {
 	private int fila;
 	private int columna;
-    private int seccion;
-	
-    public Posicion(){
-        
-    }
-    
-    public Posicion(int fila, int columna, int seccion){
-        this.fila = fila;
-        this.columna = columna;
-        this.seccion = seccion;
-    }
-    
+  private int seccion;
+
+  public Posicion(){
+
+  }
+
+  public Posicion(int fila, int columna, int seccion){
+      this.fila = fila;
+      this.columna = columna;
+		  this.seccion = seccion;
+  }
+
 	public int getFila (){
 		return this.fila;
 	}
-	
+
 	public int getColumna (){
 		return this.columna;
 	}
-	
+
 	public void setFila (int row){
 		this.fila = row;
 	}
-	
+
 	public void setColumna (int column){
 		this.columna = column;
 	}
-    
+
     public void setSeccion(int seccion){
         this.seccion = seccion;
     }
-    
+
     public int getSeccion(){
         return this.seccion;
     }
-	
+
     /*
      * Encargado de modificar la posicion actual acorde a la direccion enviada.
      * @params Direccion direccion: Direccion a utilizar para actualizar posicion
@@ -60,7 +60,7 @@ public class Posicion {
         }
 		 return this;
 	}
-    
+
     /*
      * Utilizado para comparar la posicion actual con la enviada.
      * @params Posicion posicion: Posicion con la que se la quiere comparar
--- src/modelo/dinamica/objetos/Paloma.java
+++ src/modelo/dinamica/objetos/Paloma.java
@@ -2,34 +2,50 @@ package modelo.dinamica.objetos;
 
 import modelo.direcciones.Direccion;
 import modelo.dinamica.Posicion;
+import modelo.util.RandomAcotado;
+import java.lang.IllegalArgumentException;
 
 /*
  * Representa la paloma que aparece de manera random, desplazandose desde un extremo
  * hacia al otro y desapareciendo.
  */
+
 public class Paloma extends Objeto{
     private Direccion direccion;
-    
+
     /*
-     * El constructor de la paloma recibe la direccion a la que se debe mover.
-     * Ya que se puede desplazar de Izquierda -> Derecha o Derecha -> Izquierda.
+     * El constructor de la paloma recibe la posicion desde la que parte.
+     * A partir de ello interpreta si debe ir hacia la derecha o hacia la izquierda.
+     * En caso de no poder interpretarlo por haber un error de posicionamiento (la paloma no empieza desde un costado) arroja una excepción de error.
      */
-    public Paloma(Direccion direccion){
-        this.direccion = direccion;
+
+    public Paloma (Posicion pos) {
+        this.setPosicion(pos);
+        this.setDireccion(this.smartAddress(pos));
     }
-    
+
+    private Direccion smartAddress (Posicion pos) throws IllegalArgumentException {
+      if (pos.getColumna() == 0) {
+        return Direccion.DERECHA;
+      }
+      else if (pos.getColumna() == 4) {
+        return Direccion.IZQUIERDA;
+      }
+      else throw new IllegalArgumentException("Parámetro posicional inválido");
+    }
+
     public Direccion getDireccion(){
         return this.direccion;
     }
-    
+
     public void setDireccion(Direccion direccion){
         this.direccion = direccion;
     }
-    
+
     public void actualizar(){
         desplazar();
     }
-    
+
     private void desplazar(){
         super.mover(this.getDireccion());
     }
